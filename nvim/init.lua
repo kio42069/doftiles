5fa3a2d28e699dcdc071916268133598f36d4ed9
Surat <suratsamanta@gmail.com>
2024-05-18 21:23:17 +0530

    sdakfjlsdakf

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,40 @@
-vim.defer_fn(function()
-  pcall(require, "impatient")
-end, 0)
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
+vim.g.mapleader = " "
 
--- For options & keymappings & coloring
-require("core")
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- Install the Lazy Plugins Manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"--single-branch",
-		"https://github.com/folke/lazy.nvim.git",
-		lazypath,
-	})
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
 
-vim.opt.runtimepath:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
--- Where you can intall and modify your plugins 
-require("plugins")
--- Highlighting
-require("core.hi")
+local lazy_config = require "configs.lazy"
+
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require "options"
+    end,
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+vim.wo.relativenumber = true
+require "nvchad.autocmds"
+vim.api.nvim_set_keymap('n', ':W', ':w', { noremap = true })
+vim.api.nvim_set_keymap('n', ':Q', ':q', { noremap = true })
+vim.schedule(function()
+  require "mappings"
+end)
